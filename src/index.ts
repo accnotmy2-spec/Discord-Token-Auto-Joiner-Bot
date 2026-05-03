46aab04f96f5b8d9fa2d185a5bc441b89f37003b
accnotmy2-spec <accnotmy2@gmail.com>
2026-05-03 15:40:18 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,79 @@
-import { Client, EmbedBuilder, GatewayIntentBits } from "discord.js";
-import discordSelfBot from "discord.js-selfbot-v13";
-import dotenv from "dotenv";
-import utils from "./utils";
-dotenv.config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+const axios = require('axios');
+const mongoose = require('mongoose');
+const app = express();
 
-const client = new Client({ intents: Object.keys(GatewayIntentBits) as [] });
-client.login(process.env.CLIENT_TOKEN).catch(console.log);
-
-
-client.on("ready", async () => {
-    console.log(`Logged in as ${client.user?.tag}!`);
-
-    //set command in bot
-    const data = [
-        {
-            name: "jointokens",
-            description: "join tokens to server",
-            options: [
-                {
-                    name: "guildid",
-                    description: "guild id",
-                    type: 3,
-                    required: true
-                }
-            ]
-        }
-    ];
-    client.application?.commands.set(data);
+const client = new Client({ 
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] 
 });
 
+// User Schema (Bando ka data save karne ke liye)
+const UserSchema = new mongoose.Schema({
+    userId: String,
+    accessToken: String,
+    refreshToken: String
+});
+const User = mongoose.model('User', UserSchema);
 
+// OAuth2 Callback Route
+app.get('/callback', async (req, res) => {
+    const code = req.query.code;
+    if (!code) return res.send("No code found!");
 
+    try {
+        const response = await axios.post('https://discord.com/api/oauth2/token', new URLSearchParams({
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: process.env.REDIRECT_URI
+        }));
 
-client.on("interactionCreate", async (i) => {
-    if (!i.isChatInputCommand()) return;
+        const userData = await axios.get('https://discord.com/api/users/@me', {
+            headers: { Authorization: `Bearer ${response.data.access_token}` }
+        });
 
+        // Database mein save ya update karna
+        await User.findOneAndUpdate(
+            { userId: userData.data.id },
+            { accessToken: response.data.access_token, refreshToken: response.data.refresh_token },
+            { upsert: true }
+        );
+
+        res.send("<h1>Verify Ho Gye Ho!</h1><p>Ab aap server mein add ho jayenge.</p>");
+    } catch (err) {
+        res.send("Error: " + err.message);
+    }
+});
+
+// Join Command: !join [ServerID]
+client.on('messageCreate', async (msg) => {
+    if (msg.content.startsWith('!join')) {
+        const serverId = msg.content.split(' ')[1];
+        if (!serverId) return msg.reply("Bhai, Server ID toh do!");
+
+        const allUsers = await User.find();
+        msg.reply(`${allUsers.length} users ko join karwana shuru kar raha hoon...`);
+
+        allUsers.forEach((user, index) => {
+            setTimeout(async () => {
+                try {
+                    await axios.put(
+                        `https://discord.com/api/guilds/${serverId}/members/${user.userId}`,
+                        { access_token: user.accessToken },
+                        { headers: { Authorization: `Bot ${process.env.TOKEN}` } }
+                    );
+                } catch (e) { console.log(`Failed for ${user.userId}`); }
+            }, index * 2500); // 2.5 second ka delay taake ban na ho
+        });
+    }
+});
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    client.login(process.env.TOKEN);
+    app.listen(process.env.PORT || 3000);
+});
     if (i.commandName == "jointokens") {
         const guildId = i.options.getString("guildid") as string;
         const guild = client.guilds.cache.get(guildId);
